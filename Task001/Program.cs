217b8244f328e277cf4fd720a8c54715c49b3dea
ElenaG <freshelf@yandex.ru>
2022-08-23 23:47:43 +0500

    add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,33 @@
+﻿// Перевернуть одномерный массив.
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i =0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+void ReverseArray(int[]array)
+{
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+int[] array = CreateArray(10, - 5, 11);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
