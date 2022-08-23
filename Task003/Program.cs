cc1c2873d481900c404fa1ea64caca85f81e5d9a
ElenaG <freshelf@yandex.ru>
2022-08-23 23:49:18 +0500

    add Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,33 @@
+﻿// Перевести десятичное число в двоичное.
+
+int FindArrayLength(int number)
+{
+    int length = 0;
+    while (number >= 1)
+    {
+        number = number / 2;
+        length++;
+    }
+    return length;
+}
+
+int[] ConvertToBinary(int number)
+{
+    int[] array = new int[FindArrayLength(number)];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[array.Length - 1 - i] = number % 2;
+        number = number / 2;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i =0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int[] array = ConvertToBinary(8);
+PrintArray(array);
