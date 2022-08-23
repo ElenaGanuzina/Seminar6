ffd136d63f7425bb2c2f3d56e52f52ce90ca9151
ElenaG <freshelf@yandex.ru>
2022-08-23 23:48:47 +0500

    add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,10 @@
+﻿// 40. На вход - три числа. Проверить, существует ли треугольник с такими сторонами.
+
+void ExistTriangle( int a, int b, int c)
+{
+    if(a + b > c && a + c > b && b + c > a) Console.WriteLine("Такой треугольник существует");
+    else  Console.WriteLine("Такого треугольника не существует");
+}
+
+ExistTriangle(10, 6, 15);
+ExistTriangle(6, 7, 38);
