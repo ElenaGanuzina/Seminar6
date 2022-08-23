2effbbfa8637b082732d0edaf9697ff1426eb0d7
ElenaG <freshelf@yandex.ru>
2022-08-23 23:49:36 +0500

    add Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,32 @@
+﻿// 45. Копия заданного массива, с помощью поэлементного копирования.
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i =0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] array2 = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array2[i] = array [i];
+    }
+    return array2;
+}
+
+int[] array = CreateArray(5, 0, 100);
+PrintArray(array);
+PrintArray(CopyArray(array));
