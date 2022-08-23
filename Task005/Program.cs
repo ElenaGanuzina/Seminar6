c391ecb3e75d73564a52b85756fd6a889aa19f9c
ElenaG <freshelf@yandex.ru>
2022-08-23 23:49:58 +0500

    add Task005

---
+++ Task005/Program.cs
@@ -0,0 +1,23 @@
+﻿// 44. Вывести первые N чисел Фибоначчи (без рекурсии).
+
+double Fibonacci(int n)
+{
+    double a = 1;
+    double b = 1;
+    double c = 0;
+    if (n == 0 || n == 1) return 1;
+    else
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+    return c;
+}
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"Порядковый номер - {i} = {Fibonacci(i)}");
+}
